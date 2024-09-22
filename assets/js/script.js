b5edd617c0b348650abe1d6ba1b33e0bb3fbca90
STE <rupak.manna99@gmail.com>
2024-09-23 00:06:52 +0530

    2nd push

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,52 @@ try {
 } catch (err) {
   console.log(err);
 }
+
+try {
+  function showContainer(containerId) {
+    const containers = document.querySelectorAll(".content-container");
+    containers.forEach((container) => {
+      container.classList.remove("flex");
+    });
+
+    const buttons = document.querySelectorAll(".tab-button");
+    buttons.forEach((button) => {
+      button.classList.remove("activeBtn");
+    });
+
+    const selectedContainer = document.getElementById(containerId);
+    selectedContainer.classList.add("flex");
+
+    const clickedButton = document.querySelector(
+      `button[onclick="showContainer('${containerId}')"]`
+    );
+    clickedButton.classList.add("activeBtn");
+  }
+
+  window.onload = () => {
+    showContainer("static");
+  };
+} catch (err) {
+  console.log(err);
+}
+
+try {
+  function toggleFaq(faqNumber) {
+    const faqContent = document.getElementById(`content-${faqNumber}`);
+    const faqIcon = document.getElementById(`icon-${faqNumber}`);
+    const faqContainer = document.getElementById(`faq-${faqNumber}`);
+
+    faqContent.classList.toggle("hidden");
+
+    faqIcon.classList.toggle("rotate-0");
+    faqIcon.classList.toggle("rotate-45");
+
+    if (faqContent.classList.contains("hidden")) {
+      faqContainer.classList.remove("bg-[#f0f3ff]");
+      faqContainer.classList.add("bg-white");
+    } else {
+      faqContainer.classList.remove("bg-white");
+      faqContainer.classList.add("bg-[#f0f3ff]");
+    }
+  }
+} catch (err) {}
