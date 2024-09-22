1e120c1fcbc185e0d9de496072623e2317c583f3
STE <rupak.manna99@gmail.com>
2024-09-22 10:44:45 +0530

    2nd push

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,15 @@ const mobileMenu = document.getElementById("mobile-menu");
 mobileMenuButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+try {
+  const slider = document.querySelector("#slider");
+  function moveNext() {
+    slider.scrollLeft += slider.offsetWidth;
+  }
+  function movePrev() {
+    slider.scrollLeft -= slider.offsetWidth;
+  }
+} catch (err) {
+  console.log(err);
+}
